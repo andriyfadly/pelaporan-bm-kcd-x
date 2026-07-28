5dc235478ec097e8d6529a8ba31659627602d68b
fikriks <fikriks130@gmail.com>
2026-07-28 12:46:11 +0700

    chore: hapus _test_env.php (file test temp) dari repo + gitignore

--- _test_env.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__ . '/env.php';
-echo 'HOST=[' . getenv('DB_HOST') . "]\n";
-echo 'USER=[' . getenv('DB_USER') . "]\n";
-echo 'PASS=[' . getenv('DB_PASS') . "]\n";
-echo 'PASS_LEN=' . strlen(getenv('DB_PASS')) . "\n";
